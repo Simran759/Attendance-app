21c44de8660588f4eb8cc9fa9816c0b75de8a09d
Simran759 <vanshikab458@gmail.com>
2025-03-29 01:29:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,9 +188,9 @@ with app.app_context():
     db.create_all()
 
 # Run Flask App
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5000)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=False)
+    app.run(debug=True, port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=10000, debug=False)
 
 
