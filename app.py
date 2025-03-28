abcf94deef5f5e7332db84dedc997a419598648c
Simran759 <vanshikab458@gmail.com>
2025-03-29 01:55:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -187,11 +187,11 @@ def get_attendance():
 # Initialize Database
 with app.app_context():
     db.create_all()
-
-# Run Flask App
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+# Run Flask App
 # if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=10000, debug=False)
+#     app.run(debug=True, port=5000)
+
 
 
