82431fb327af5bc6a77ad060675d82f0642d6be5
Simran <vanshikab458@gmail.com>
2025-04-04 15:30:11 +0530

    change env variable

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Database Configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Initialize database
